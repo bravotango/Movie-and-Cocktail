98cecf74ca898d7f08256ab898c81cac9ac3328f
JonkHunkle <dczerwinski@ymail.com>
2021-10-21 21:33:20 -0700

    movie api slap and ?task list?

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,43 @@
+//~~~drinks are pulled from cocktail api slap and turned into cocktail object~~~
+
+//input movie name
+
+//click the button
+//sammy
+
+//slaps movie api and saves input to local storage
+//David
+//pulls genre, poster, rating
+
+// with the genre we pull from cocktail object with suggested drinks
+
+//create <aside> HTML with movie title, poster, rating and suggested drink with image
+
 $(document).ready(function () {
-  const omdbApiKey = "";
-  const theCocktailDbKey = "";
+  function movieSearch() {
+    const movieTitle = $("#userQuery").val();
+    getMovie(movieTitle);
+  }
+
+  function getMovie(movie) {
+    const omdbApiKey = "dc038d01";
+    let requestMovieURL = `https://www.omdbapi.com/?apikey=${omdbApiKey}&t=${movie}`;
+    console.log(movie);
+    // fetch
+    fetch(requestMovieURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  }
+
+  function getCocktail(cocktail) {
+    console.log(cocktail);
+    // fetch
+  }
+
+  getMovie("The Big Lebowski");
+  getCocktail("White Russian");
 });
-
-function getMovie(movie) {
-  console.log(movie);
-  // fetch
-}
-
-function getCocktail(cocktail) {
-  console.log(cocktail);
-  // fetch
-}
-
-getMovie("The Big Lebowski");
-getCocktail("White Russian");
